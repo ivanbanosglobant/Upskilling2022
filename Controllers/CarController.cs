7ab8df9bf52e5585055d49182a93722833b45c6d
Ivan Baños <ivan.banos@globant.com>
2022-08-19 08:49:50 -0500

    Task1 created project

---
+++ Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using ExampleWebApiNetCore6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ExampleWebApiNetCore6.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarController : ControllerBase
+    {
+        [HttpPost]
+        [Route("AddCar")]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> AddCar(Car car)
+        {
+            return Ok($"Car {JsonConvert.SerializeObject(car)} Added");
+        }
+
+        [HttpGet("GetCar/{plate}")]
+        [ProducesResponseType(typeof(Car), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetCar(string plate)
+        {
+            var car = new Car() { Plate = "KJZ881", User = "Ivan Baños" };
+            return Ok(car);
+        }
+    }
+}
